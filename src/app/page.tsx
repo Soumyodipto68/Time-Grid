2f35893660b9b07bc6a2551d32b06d6ac52de164
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-08-21 20:45:04 +0530

    Add WorkingHoursTimeline component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import LocalTimeGrid from "../components/LocalTimeGrid";
 import Sidebar from "../components/Sidebar";
+import WorkingHoursTimeline from "../components/WorkingHoursTimeline";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
         <Header />
 
         <LocalTimeGrid />
+
+        <WorkingHoursTimeline />
       </section>
     </main>
   );
